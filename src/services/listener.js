e0694e3957624223727c8f1f00acda70a1e06781
Bruno Rossini <brunorossini@live.com>
2020-05-11 17:09:09 -0300

    fix buffer positions

--- src/services/listener.js
+++ src/services/listener.js
@@ -6,15 +6,16 @@ let listener = function() {
     .createServer(socket => {
       socket.setEncoding("utf8");
       socket.on("data", async data => {
-        if(data.indexOf('007503739') !== -1)
+        if(data.indexOf('007503739') !== -1)  
           console.log(data)
         let position;
         if (data.indexOf("$") >= -1) {
           position = data.split("$")[0];
+          Worker.run(position)
         } else {
-          position = data.split("\r")[0];
+          positions = data.split("\r");
+          positions.map(position => Worker.run(position))
         }
-        Worker.run(position)
       });
 
       socket.on("error", function(err) {
@@ -28,3 +29,15 @@ let listener = function() {
 };
 
 module.exports = listener;
+
+
+
+
+20200511;19:25:46
+20200511;19:40:46
+20200511;19:38:16
+20200511;19:35:46
+20200511;19:33:16
+20200511;19:31:16
+20200511;19:28:46
+20200511;19:26:16
