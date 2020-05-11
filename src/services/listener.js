814c004ab1fe245dff3e505ca9f7d361d9c46d07
Bruno Rossini <brunorossini@live.com>
2020-05-11 17:10:24 -0300

    fix buffer positions

--- src/services/listener.js
+++ src/services/listener.js
@@ -1,25 +1,24 @@
 const net = require("net");
-let Worker = require("../jobs/Worker")
+let Worker = require("../jobs/Worker");
 
-let listener = function() {
+let listener = function () {
   net
-    .createServer(socket => {
+    .createServer((socket) => {
       socket.setEncoding("utf8");
-      socket.on("data", async data => {
-        if(data.indexOf('007503739') !== -1)  
-          console.log(data)
+      socket.on("data", async (data) => {
+        if (data.indexOf("007503739") !== -1) console.log(data);
         let position;
         if (data.indexOf("$") >= -1) {
           position = data.split("$")[0];
-          Worker.run(position)
+          Worker.run(position);
         } else {
           positions = data.split("\r");
-          positions.map(position => Worker.run(position))
+          positions.map((position) => Worker.run(position));
         }
       });
 
-      socket.on("error", function(err) {
-        console.error('Connection error: ' + err);
+      socket.on("error", function (err) {
+        console.error("Connection error: " + err);
         console.log("\n Cliente desconectao por erro ");
       });
     })
@@ -29,15 +28,3 @@ let listener = function() {
 };
 
 module.exports = listener;
-
-
-
-
-20200511;19:25:46
-20200511;19:40:46
-20200511;19:38:16
-20200511;19:35:46
-20200511;19:33:16
-20200511;19:31:16
-20200511;19:28:46
-20200511;19:26:16
