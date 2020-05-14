c0a50454fe28d5e987c166b7ff8ad01ff2929569
Bruno Rossini <brunorossini@live.com>
2020-05-14 16:09:46 -0300

    socket

--- src/index.js
+++ src/index.js
@@ -2,14 +2,7 @@ require("dotenv").config();
 let listener = require("./services/listener");
 let listenerUDP = require("./services/udp");
 
-const sequelize = require("../src/config/sequelize");
-const queries = require("../src/queries");
-
-// sequelize
-//   .query(queries.findLocation(-20.351288, -40.655862), {
-//     type: sequelize.QueryTypes.SELECT
-//   })
-//   .then(result => console.log(result[0].address));
+require("events").EventEmitter.defaultMaxListeners = 99999;
 
 listener();
 listenerUDP();
