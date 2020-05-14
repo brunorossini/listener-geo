d24819f85cbca621944e6b9613819aca7d8564fa
Bruno Rossini <brunorossini@live.com>
2020-05-14 16:36:47 -0300

    socket

--- src/services/listener.js
+++ src/services/listener.js
@@ -1,7 +1,7 @@
 const net = require("net");
 let Worker = require("../jobs/Worker");
 const carrier = require("carrier");
-const io = require("socket.io")(80);
+const io = require("socket.io")(9999);
 
 let listener = function () {
   net
