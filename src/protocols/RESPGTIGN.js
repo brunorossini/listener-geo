37dda44c14e677402a3cbd5353eb15069d077b5a
Bruno Rossini <brunorossini@live.com>
2020-08-24 11:28:11 -0300

    test sleep mode

--- src/protocols/RESPGTIGN.js
+++ src/protocols/RESPGTIGN.js
@@ -1,6 +1,6 @@
 const moment = require("moment");
 
-module.exports = data => {
+module.exports = (data) => {
   return {
     header: data[0],
     firmware: data[1],
@@ -13,7 +13,7 @@ module.exports = data => {
     altitude: parseInt(data[8]),
     lng: data[9],
     lat: data[10],
-    date: moment.utc(data[11], "YYYYMMDDHHmmss"),
+    date: moment.utc(data[19], "YYYYMMDDHHmmss"),
     // date: moment(data[11], "YYYYMMDDHHmmss")
     //   .format("YYYY-MM-DD")
     //   .toString(),
@@ -25,9 +25,9 @@ module.exports = data => {
     // field16 reserved
     hour_meter: data[17],
     odometer: parseFloat(data[18]) * 1000,
-    send_time: data[19],
+    // send_time: data[19],
     count_number: data[20],
-    ignition: true
+    ignition: true,
   };
 };
 
