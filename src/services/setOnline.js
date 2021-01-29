1d6515944c7dd379d6fa4555398c99597fbe919a
Bruno Rossini <brunorossini@live.com>
2021-01-29 14:01:47 -0300

    add KeyExpire

--- src/services/setOnline.js
+++ src/services/setOnline.js
@@ -3,10 +3,6 @@ const redis = require("redis");
 const pub = redis.createClient();
 
 const setOnline = (tracker_id, position) => {
-  pub.get(`offline:${tracker_id}`, (err, reply) => {
-    if (reply) pub.del(`offline:${tracker_id}`);
-  });
-
   const key = `online:${tracker_id}`;
   pub.set(key, JSON.stringify(position));
   pub.expire(key, 60 * 60);
